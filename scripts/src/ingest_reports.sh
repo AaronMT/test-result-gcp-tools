a961b41efb943fdfa39f05d0ffeafb43bf807cbf
Aaron Train <aaron.train@gmail.com>
2024-10-09 10:49:28 -0400

    Update ingest script

--- scripts/src/ingest_reports.sh
+++ scripts/src/ingest_reports.sh
@@ -10,7 +10,7 @@ fi
 CURRENT_DATE=$(date +%Y-%m-%d)
 
 # Execute the curl command and capture the output and exit status
-response=$(curl -v -L -X POST "https://$REPORT_PORTAL_API_ENDPOINT/api/v1/$REPORT_PORTAL_PROJECT_NAME/launch/import" \
+response=$(curl -v -L -X POST "http://$REPORT_PORTAL_API_ENDPOINT/api/v1/$REPORT_PORTAL_PROJECT_NAME/launch/import" \
   -H 'Content-Type: multipart/form-data' \
   -H "Authorization: Bearer $REPORT_PORTAL_API_TOKEN" \
   -F 'file=@'"$ZIP_FILE"';type=application/x-zip-compressed' \
